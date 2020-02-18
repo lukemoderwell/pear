64ac65237d174d89842bc541252523abde709c61
Luke Moderwell <luke.moderwell@gmail.com>
2020-02-18 09:13:09 -0500

    write .env file on the fly

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,30 @@ const webpack = require('webpack');
 const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const fs = require('fs');
 
 module.exports = env => {
   let envKeys = {};
   // call dotenv and it will return an Object with a parsed key
   if (env.production === true) {
     console.log('production build');
-  } else {
-    const result = dotenv.config();
-
-    if (result.error) {
-      throw result.error;
-    }
-
-    const localEnv = env.production
-      ? process.env.AIRTABLE_API_KEY
-      : result.parsed;
-
-    // reduce it to a nice object, the same as before
-    envKeys = Object.keys(localEnv).reduce((prev, next) => {
-      prev[`process.env.${next}`] = JSON.stringify(localEnv[next]);
-      return prev;
-    }, {});
+    const key = `AIRTABLE_API_KEY=${process.env.AIRTABLE_API_KEY}`;
+    fs.writeFileSync('./.env', key);
+    console.log(key);
   }
+  const result = dotenv.config();
+
+  if (result.error) {
+    throw result.error;
+  }
+
+  const localEnv = result.parsed;
+
+  // reduce it to a nice object, the same as before
+  envKeys = Object.keys(localEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(localEnv[next]);
+    return prev;
+  }, {});
   return {
     plugins: [
       new webpack.DefinePlugin(envKeys),
@@ -35,6 +36,7 @@ module.exports = env => {
           charset: { charset: 'utf-8' },
           viewport: 'width=device-width, initial-scale=1'
         },
+        inject: true,
         template: './src/index.html'
       })
     ],
